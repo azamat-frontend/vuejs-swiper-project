184963e5985e80561d120e23b3fa101683ffeba4
Azamat <azamatfrontend@gmail.com>
2022-04-24 05:51:22 +0500

    keep calm and commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomeHeader from '@/components/HomeHeader'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/assets/scss/main.scss';
 </style>
---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="home-header">
+    <div class="top-line">
+      <a class="logo" href="/">
+        <span class="logo__image-wrapper">
+          <img class="logo__svg" src="@/assets/images/logo.svg" alt="" />
+        </span>
+        <span class="logo__text">Азамат</span>
+      </a>
+      <div class="top-line__decoration"></div>
+      <div class="top-line__contacts">
+        <span>Телефон:</span> <a class="top-line__number" href="tel:+998(95)004-40-48">+998(95)004-40-48</a>
+        <span class="top-line__separator"></span>
+        <span class="top-line__address-wrapper">
+          <span>Адрес:</span><a class="top-line__address" href="https://maps.google.com/maps?q=41.364945,69.199264&ll=41.364945,69.199264&z=16" target="_blank">ул. Ташкент, 37</a>
+        </span>
+      </div>
+      <div class="top-line__decoration"></div>
+      <div class="menu-button-container">
+        <div class="menu-button"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
